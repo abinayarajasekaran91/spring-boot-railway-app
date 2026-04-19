7c6880041aab1591bc9da8b12156cad0ffdebce1
Abinaya <abinaya.rajasekaran@fundsindia.com>
2026-04-19 13:12:04 +0530

    branch updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting DemoApplication");
+		System.out.println("Starting DemoApplication....");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
